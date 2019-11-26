81317ca4e28f40673620ccfbfb3fb303c69dd75d
JJ Garlitos <jj.garlitos@outlook.com>
2019-11-25 20:58:15 -0700

    Wired adding lists

--- app/Controllers/ListController.js
+++ app/Controllers/ListController.js
@@ -1,13 +1,32 @@
 import ListService from "../Services/ListService.js";
+import store from "../store.js";
 
 //TODO Don't forget to render to the screen after every data change.
-function _drawLists() {}
+function _drawLists() {
+  let template = "";
+  let lists = store.State.lists;
+  lists.forEach(l => {
+    console.log(l);
+  });
+}
+
+_drawLists();
 
 //Public
 export default class ListController {
-  constructor() {
-    //NOTE: After the store loads, we can automatically call to draw the lists.
+  addList() {
+    console.log("I'm in the controller...");
+
+    let fakeList = {
+      title: "ToDo"
+    };
+    ListService.addList(fakeList);
+
     _drawLists();
+    // constructor() {
+    //   //NOTE: After the store loads, we can automatically call to draw the lists.
+    //   _drawLists();
+    // }
   }
 
   //TODO: Your app will need the ability to create, and delete both lists and listItems
--- app/Services/ListService.js
+++ app/Services/ListService.js
@@ -1,11 +1,17 @@
 import List from "../Models/List.js";
+import store from "../store.js";
 
 //Public
 class ListService {
+  addList(fakeList) {
+    store.State.lists.push(fakeList);
+  }
+  constructor() {}
   //TODO  Here is where we handle all of our business logic,
   //given the information you need in the controller,
   //what methods will you need to do when this class is first 'constructed'?
-  //NOTE You will need this code to persist your data into local storage, be sure to call the store method to save after each change
+  //NOTE You will need this code to persist your data into local storage,
+  //be sure to call the store method to save after each change
 }
 
 const SERVICE = new ListService();
